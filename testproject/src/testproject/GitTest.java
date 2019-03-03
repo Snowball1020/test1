53ba8cc56ab6abcc51e4e8c707c43751f07d3120
Yuki <adohiito@gmail.com>
2019-03-03 18:23:12 -0500

    First Commit

---
+++ testproject/src/testproject/GitTest.java
@@ -0,0 +1,17 @@
+package testproject;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hello();
+		
+		
+	}
+
+	public static void Hello(){
+		System.out.println("Hi Git");
+	}
+	
+}
