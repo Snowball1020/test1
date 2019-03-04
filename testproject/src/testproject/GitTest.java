5a76333e06c97a1ebe7186098fe7b08f1520cfb6
Yuki <adohiito@gmail.com>
2019-03-03 19:00:41 -0500

    Added Dance and Multiply Methods

--- testproject/src/testproject/GitTest.java
+++ testproject/src/testproject/GitTest.java
@@ -6,14 +6,27 @@ public class GitTest {
 		// TODO Auto-generated method stub
 
 		Hello();
-		
+		Dance(3);
+		Multiply(2.5,5.5);
 		
 	}
 
 	public static void Hello(){
 		System.out.println("Hi Git This is from yuki");
-		System.out.println("Can you hear me?");
+		System.out.println("Can you hear me? Again");
 
 	}
+	
+	public static void Dance(int times){
+		
+		System.out.println("We dance for " + times + "times today" );
+		
+	}
+	
+	public static double Multiply(double d, double e){
+		double x = d * e;
+		return x;
+	}
+	
 	
 }
