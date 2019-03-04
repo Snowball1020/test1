b9a7f3cfc068ca815fa3bb4e7ecbee8938f4be14
Yuki <adohiito@gmail.com>
2019-03-03 19:05:48 -0500

    Project1(Hello,Dance,and Multiply)

--- testproject/src/testproject/GitTest.java
+++ testproject/src/testproject/GitTest.java
@@ -7,19 +7,20 @@ public class GitTest {
 
 		Hello();
 		Dance(3);
-		Multiply(2.5,5.5);
+		double multiply = Multiply(2.5,5.5);
+		System.out.printf("Calculation result is: %.2f\n" ,multiply);
 		
 	}
 
 	public static void Hello(){
 		System.out.println("Hi Git This is from yuki");
-		System.out.println("Can you hear me? Again");
+		System.out.println("Can you hear me?");
 
 	}
 	
 	public static void Dance(int times){
 		
-		System.out.println("We dance for " + times + "times today" );
+		System.out.println("We dance for " + times + " times today" );
 		
 	}
 	
